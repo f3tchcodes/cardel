6a727b31ae49dd9f83bbf54e128f39844d85a83d
f3tch <f3tch.169@gmail.com>
2026-05-29 06:25:26 +0500

    basic backend for account settings: email, password

--- routes/APIs/user_settings.js
+++ routes/APIs/user_settings.js
@@ -10,6 +10,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const argon2 = require("argon2");
 
 const multer = require("multer");
 const sharp = require("sharp");
@@ -147,6 +148,156 @@ router.post("/profile", userPfpUpload.single("sub_icon"), async (req, res) => {
         status: true,
         message: "Successfully updated the account!"
     });
+
+});
+
+router.post("/account", async (req, res) => {
+    try {
+        jwt_data = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
+    } catch (err) {
+        console.log(err);
+        return res.json({
+            error: true,
+            message: "JWT can't be verified, login again or contact us to fix.",
+        });
+    }
+
+    let rows1;
+
+    try {
+        [rows1] = await con.query(
+            `
+            SELECT * 
+            FROM users
+            WHERE email = ?;`,
+            [jwt_data.email],
+        );
+
+        if (rows1.length === 1) {
+            if (rows1[0].first_time_login === 1) {
+                return res.redirect("/onboarding");
+            }
+        } else {
+            res.clearCookie("token");
+            return res.json({
+                error: true,
+                message:
+                    "Either user does not exist or there are multiple users with this email, clear cookies and login again. If it does not work, contact us at our support email.",
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        return res.json({
+            error: true,
+            message:
+                "Error occured, request did not go through. Contact support to get help.",
+        });
+    }
+
+    const user = rows1[0];
+
+    const { email, current_password, new_password } = req.body; 
+    
+    // email validation
+    const normalizedEmail = email.toLowerCase().trim();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (normalizedEmail !== user.email) {
+        // basic checks of email
+
+        if (normalizedEmail.length > 100) {
+            return res.json({
+                success: false,
+                error: true,
+                message: "Input data is too large.",
+            });
+        }
+
+        if (!emailRegex.test(normalizedEmail)) {
+            return res.json({
+                success: false,
+                error: true,
+                message: "Please enter a valid email address.",
+            });
+        }
+
+        const [emailResults] = await con.query(
+            `
+            UPDATE users
+            SET email = ?
+            WHERE user_id = ?
+            `,
+            [normalizedEmail, jwt_data.user_id]
+        )
+
+        if (emailResults.affectedRows === 0){
+            return res.json({
+                success: false,
+                error: true,
+                message: "Error occured. Email has not been updated!"
+            })
+        }
+    }
+
+    // password verification and validation
+    if (current_password > 0 || new_password > 0) {
+        let isPasswordCorrect;
+
+        // password verification
+        try {
+            isPasswordCorrect = await argon2.verify(user.password, current_password);
+        } catch (err) {
+            console.log(err);
+            return res.json({
+                success: false,
+                error: true,
+                message: "Argon could not verify the password: contact support.",
+            });
+        }
+
+        if (!isPasswordCorrect) {
+            // password is not correct
+            return res.json({
+                success: false,
+                error: true,
+                message: "Current password is not correct!",
+            });
+        } else {
+            // password has been verified, continue from here
+            // password validation
+            if (new_password.length < 8) {
+                return res.json({
+                    success: false,
+                    error: true,
+                    message: "Password must be 8 or more characters.",
+                });
+            }
+
+            const hashed_password = await argon2.hash(new_password);
+            
+            const [passwordResults] = await con.query(
+                `
+                UPDATE users
+                SET password = ?
+                WHERE user_id = ?
+                `,
+                [current_password, jwt_data.user_id]
+            );
+
+            if (passwordResults.affectedRows === 0){
+                return res.json({
+                    success: false,
+                    error: true,
+                    message: "Error occured. Password has not been updated!"
+                })
+            }
+        }
+    }
+
+    return res.status(200).json({
+        success: true,
+        message: "Account settings updated successfully!"
+    })
 
 });
 
