f2168bcd5b552e7f57f110e2a78dff0fb53ff0f5
f3tch <f3tch.169@gmail.com>
2026-05-30 23:06:32 +0500

    variables and basic setup

---
+++ routes/APIs/analytics.js
@@ -0,0 +1,29 @@
+/*
+    ROUTE: /api/user/analytics
+
+    ENDPOINTS:
+    GET  -- /current_month
+    POST -- /date  -- (from, to)
+*/
+
+const express = require("express");
+const router = express.Router();
+
+router.post("/current_month", async (req, res) => {
+    let
+        current_month_cost,
+        next_payment,
+        biggest_cost,
+        week_1,
+        week_2,
+        week_3,
+        week4,
+        category_streaming,
+        category_gaming,
+        category_workbusiness,
+        category_health,
+        category_digitaltools,
+        category_others;
+});
+
+module.exports = router;
