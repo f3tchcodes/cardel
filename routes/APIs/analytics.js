533233a1b9a2e79ee9cb067bdac8cb2c8f4b8ca7
f3tch <f3tch.169@gmail.com>
2026-05-31 14:59:18 +0500

    basic jwt setup

--- routes/APIs/analytics.js
+++ routes/APIs/analytics.js
@@ -8,11 +8,56 @@
 
 const express = require("express");
 const router = express.Router();
+const con = require("@utils/database");
+const jwt = require("jsonwebtoken");
 
 router.get("/current_month", async (req, res) => {
+    let jwt_data;
+    
+    try {
+        jwt_data = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
+    } catch (err) {
+        console.log(err);
+        return res.json({
+            error: true,
+            message: "JWT can't be verified, login again or contact us to fix.",
+        });
+    }
+
+    try {
+        [rows1] = await con.query(
+            `
+            SELECT * 
+            FROM users
+            WHERE email = ?;`,
+            [jwt_data.email],
+        );
+
+        if (rows1.length === 1) {
+            if (rows1[0].first_time_login === 1) {
+                return res.redirect("/onboarding");
+            }
+        } else {
+            res.clearCookie("token");
+            console.log(err);
+            return res.json({
+                error: true,
+                message:
+                    "Refresh page!",
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        return res.json({
+            error: true,
+            message:
+                "Error occured, request did not go through. Contact support to get help.",
+        });
+    }
+
     // initializing variables
     let
-        current_month_cost,
+        current_month_cost = 0,
         next_payment,
         biggest_cost,
         week_1,
@@ -25,13 +70,6 @@ router.get("/current_month", async (req, res) => {
         category_health,
         category_digitaltools,
         category_others;
-
-    const now = new Date();
-    const firstDayCurrentMonth = new Date(now.getFullYear(), now.getMonth(), 1);
-    const firstDayNextMonth = new Date(now.getFullYear(), now.getMonth() + 1, 1); // <-- This one
-
-    console.log(firstDayNextMonth);
-    console.log(firstDayCurrentMonth); 
 });
 
 module.exports = router;
